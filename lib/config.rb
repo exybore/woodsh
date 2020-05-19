7571c914c0903b6e954f6f66d87a4a3d8184de02
Théo Vidal <exybore@becauseofprog.fr>
2020-05-19 22:19:18 +0200

    feat: add data category to config

--- lib/config.rb
+++ lib/config.rb
@@ -3,17 +3,20 @@ require 'yaml'
 module Woodsh
   # The app's configuration object
   class Config
+    # @return [Array] Custom commands for the user
+    attr_reader :custom_commands
+
     # @return [Hash<String>] Theme of the shell
     attr_reader :theme
 
-    # @return [Array] Custom commands for the user
-    attr_reader :custom_commands
+    # @return [Hash<String>] Additional data to be passed in the config (e.g: module configuration)
+    attr_reader :data
 
     # Initialize the config
     #
     # @param config_path [String] path to the config
     def initialize(config_path)
-      files = ['custom_commands', 'theme']
+      files = ['custom_commands', 'data', 'theme']
       for file in files
         data = File.open("#{config_path}/#{file}.yml").read
         parsed_data = ::YAML.load(data)
