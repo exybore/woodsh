6a9dbeb650863b53a11845c60bc72fb999f4fe3f
Théo Vidal <exybore@becauseofprog.fr>
2020-05-19 22:20:32 +0200

    feat: add weather module
    Added a module to get weather data for a specific location. This works using the openweathermap gem.

---
+++ src/modules/weather.rb
@@ -0,0 +1,24 @@
+require 'openweathermap'
+require_relative 'module'
+
+API = OpenWeatherMap::API.new(Woodsh::CONFIG.data['openweathermap_key'], 'en', 'metric')
+
+module Woodsh
+  module Modules
+    # Retrieve weather informationruby on a specific location.
+    # This command uses the openweathermap gem
+    class Weather < Module
+      def self.exec(args)
+        begin
+          data = ::API.current(args)
+        rescue OpenWeatherMap::Exceptions::UnknownLocation
+          puts 'Error while getting weather data : unknown location'
+        rescue OpenWeatherMap::Exceptions::Unauthorized
+          puts "Error while getting weather data : you're not allowed to interact with the OpenWeatherMap API. Check your API key"
+        else
+          puts "#{data.weather_conditions.emoji}  #{data.city.name}, #{data.city.country} : #{data.weather_conditions.description}"
+        end
+      end
+    end
+  end
+end
